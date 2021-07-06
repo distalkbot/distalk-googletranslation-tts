4603f9aae97e717e3589d8b16f3cff65c71eee05
akira-splatoon <sa0731.sp@gmail.com>
2021-07-07 02:40:57 +0900

    Remove underscore from emoji reading name

--- discordbot.py
+++ discordbot.py
@@ -67,7 +67,8 @@ async def on_message(message):
             pattern = r'<:([a-zA-Z0-9_]+):\d+>'
             match = re.findall(pattern, text)
             for emoji_name in match:
-                text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_name}、', text)
+                emoji_read_name = emoji_name.replace('_',' ')
+                text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
             pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
             text = re.sub(pattern, '画像', text)
             pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
