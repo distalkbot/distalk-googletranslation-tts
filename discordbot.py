b2c28489e55a69f4c85bc20df8808e0551a882cb
Enosk <44186843+Nokking@users.noreply.github.com>
2022-11-11 04:37:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,15 +8,15 @@ import re
 
 import ctypes
 import ctypes.util
- 
+
 print("ctypes - Find opus:")
 a = ctypes.util.find_library('opus')
 print(a)
- 
+
 print("Discord - Load Opus:")
 b = discord.opus.load_opus(a)
 print(b)
- 
+
 print("Discord - Is loaded:")
 c = discord.opus.is_loaded()
 print(c)
